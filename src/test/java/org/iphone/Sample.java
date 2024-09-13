5a065f50570f1c9fe88988ec053ba8a567925113
anusharebala <anusharebala@gmail.com>
2024-09-13 20:42:55 +0530

    code added

--- src/test/java/org/iphone/Sample.java
+++ src/test/java/org/iphone/Sample.java
@@ -10,5 +10,15 @@ public class Sample {
 	public void framework() {
 		System.out.println("framework");
 	}
-
+	
+	public void cucumber() {
+		System.out.println("cucumber");
+	}
+	public void sql() {
+		System.out.println("sql");
+	}
+	public void company() {
+		System.out.println("company");
+	}
+	
 }
