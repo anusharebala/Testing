b0a3999b5027eb499337832fe12b11ccca15ab0a
anusharebala <anusharebala@gmail.com>
2024-09-13 20:21:49 +0530

    started

---
+++ src/test/java/org/iphone/Sample.java
@@ -0,0 +1,14 @@
+package org.iphone;
+
+public class Sample {
+	public void java() {
+		System.out.println("java");
+	}
+	public void selenium() {
+		System.out.println("selenium");
+	}
+	public void framework() {
+		System.out.println("framework");
+	}
+
+}
